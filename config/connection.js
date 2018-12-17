5364baa925759441e922d60e292e2764f402724a
EAWehner <elizabethawehner@gmail.com>
2018-12-17 15:54:45 -0500

    m

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
   host: "us-cdbr-iron-east-01.cleardb.net",
   port: 3306,
   user: "b7ba7408e70501",
-  password: "5486c88b",
+  password: "5486c66b",
   database: "heroku_60d4c191a47f5a4"
 });
 
