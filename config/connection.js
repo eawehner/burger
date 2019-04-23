86c1290a01a9ac881a0418ae1050ca8d147a95ef
eawehner <elizabethawehner@gmail.com>
2019-04-23 18:20:09 -0400

    fresh heroku deploy

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,19 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-<<<<<<< HEAD
-  //don't forget to make adjustments if you're running on your own machine or deploying elsewhere!
-  host: "localhost",
-  // port: 3306,
-=======
-  host: "localhost",
-  //commented out for heroku deploy
-  //port: 3306,
-  //insert your credentials and host here
->>>>>>> ad4e601d7a469bfd4788c6e89a5d94c67ef2d4f1
-  user: "",
-  password: "",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    //don't forget to make adjustments if you're running on your own machine or deploying elsewhere!
+    host: "localhost",
+    port: 3306,
+    //insert your credentials and host here
+    user: "",
+    password: "",
+    database: "burgers_db"
+  });
+};
 
 connection.connect(function(err) {
   if (err) {
